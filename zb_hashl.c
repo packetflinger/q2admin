f4c01a1b826fdb82a305e1bc4f1abe8d4ea54fd8
Joe Reid <joe@joereid.com>
2016-11-02 06:18:28 -0400

    formatting...

--- zb_hashl.c
+++ zb_hashl.c
@@ -16,7 +16,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-*/
+ */
 
 //
 // q2admin
@@ -29,39 +29,35 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "g_local.h"
 #include "fopen.h"
 
-qboolean ReadRemoteHashListFile(char *bfname, char *blname)
-{
+qboolean ReadRemoteHashListFile(char *bfname, char *blname) {
     URL_FILE *handle;
     FILE *outf;
 
     sprintf(buffer, "%s/%s", moddir, blname);
 
     // copy from url line by line with fgets //
-    outf=fopen(buffer,"w");
-    if(!outf)
-    {
-	gi.dprintf ("Error opening local hash list file.\n");
-	return FALSE;
+    outf = fopen(buffer, "w");
+    if (!outf) {
+        gi.dprintf("Error opening local hash list file.\n");
+        return FALSE;
     }
 
     handle = url_fopen(bfname, "r");
-    if(!handle)
-    {
-	gi.dprintf ("Error opening remote hash list file.\n");
-	fclose(outf);
-	return FALSE;
+    if (!handle) {
+        gi.dprintf("Error opening remote hash list file.\n");
+        fclose(outf);
+        return FALSE;
     }
 
-    while(!url_feof(handle))
-    {
-        if(!url_fgets(buffer,sizeof(buffer),handle)) {
-		// if it did timeout we are not trying again forever... - hifi
-		gi.dprintf("Timeout while waiting for reply.\n");
-		url_fclose(handle);
-		fclose(outf);
-		return FALSE;
-	}
-        fwrite(buffer,1,strlen(buffer),outf);
+    while (!url_feof(handle)) {
+        if (!url_fgets(buffer, sizeof (buffer), handle)) {
+            // if it did timeout we are not trying again forever... - hifi
+            gi.dprintf("Timeout while waiting for reply.\n");
+            url_fclose(handle);
+            fclose(outf);
+            return FALSE;
+        }
+        fwrite(buffer, 1, strlen(buffer), outf);
     }
 
     url_fclose(handle);
@@ -71,55 +67,47 @@ qboolean ReadRemoteHashListFile(char *bfname, char *blname)
 
 // download up to date anticheat config file including all execptions for r1ch
 // ugly code.. ;x
-void getR1chHashList(char *hashname)
-{
+
+void getR1chHashList(char *hashname) {
 
     char cfgHashList_enabled[100];
     q2a_strcpy(cfgHashList_enabled, q2adminhashlist_enable->string);
 
-        if ( cfgHashList_enabled[0] == '1')
-        {
-	    qboolean ret;
-            char cfgHashRemoteList[100];
+    if (cfgHashList_enabled[0] == '1') {
+        qboolean ret;
+        char cfgHashRemoteList[100];
 
-            if(!q2adminhashlist_dir || isBlank(q2adminhashlist_dir->string))
-                    {
-                        q2a_strcat(q2a_strcat(q2a_strcpy(cfgHashRemoteList, HASHLISTREMOTEDIR), "/"), hashname);
-                    }
-            else
-                    {
-                        q2a_strcat(q2a_strcat(q2a_strcpy(cfgHashRemoteList, q2adminhashlist_dir->string), "/"), hashname);
-                    }
-            ret = ReadRemoteHashListFile(cfgHashRemoteList, hashname);
-
-            if(!ret)
-                {
-                        gi.dprintf ("WARNING: " HASHLISTREMOTEDIR " could not be found\n");
-                        logEvent(LT_INTERNALWARN, 0, NULL, HASHLISTREMOTEDIR " could not be found", IW_BANSETUPLOAD, 0.0);
-                }
+        if (!q2adminhashlist_dir || isBlank(q2adminhashlist_dir->string)) {
+            q2a_strcat(q2a_strcat(q2a_strcpy(cfgHashRemoteList, HASHLISTREMOTEDIR), "/"), hashname);
+        } else {
+            q2a_strcat(q2a_strcat(q2a_strcpy(cfgHashRemoteList, q2adminhashlist_dir->string), "/"), hashname);
         }
+        ret = ReadRemoteHashListFile(cfgHashRemoteList, hashname);
+
+        if (!ret) {
+            gi.dprintf("WARNING: " HASHLISTREMOTEDIR " could not be found\n");
+            logEvent(LT_INTERNALWARN, 0, NULL, HASHLISTREMOTEDIR " could not be found", IW_BANSETUPLOAD, 0.0);
+        }
+    }
 }
 
-void loadhashlist(void)
-{
+void loadhashlist(void) {
     char cfgHashList_enabled[100];
     q2a_strcpy(cfgHashList_enabled, q2adminhashlist_enable->string);
-        if ( cfgHashList_enabled[0] == '1')
-	{
-	    // flush cache, which really sux
-            //q2a_strcpy(buffer, "fsflushcache\n");
-	    //gi.AddCommandString(buffer);
-	    getR1chHashList("anticheat-cvars.txt");
-	    getR1chHashList("anticheat-hashes.txt");
-	    getR1chHashList("anticheat-tokens.txt");
-    	    // load hash list (is there a command to do this? else load on restart
-            q2a_strcpy(buffer, "svacupdate\n");
-	    gi.AddCommandString(buffer);
-	}
+    if (cfgHashList_enabled[0] == '1') {
+        // flush cache, which really sux
+        //q2a_strcpy(buffer, "fsflushcache\n");
+        //gi.AddCommandString(buffer);
+        getR1chHashList("anticheat-cvars.txt");
+        getR1chHashList("anticheat-hashes.txt");
+        getR1chHashList("anticheat-tokens.txt");
+        // load hash list (is there a command to do this? else load on restart
+        q2a_strcpy(buffer, "svacupdate\n");
+        gi.AddCommandString(buffer);
+    }
 }
 
-void reloadhashlistRun(int startarg, edict_t *ent, int client)
-{
+void reloadhashlistRun(int startarg, edict_t *ent, int client) {
     loadhashlist();
-    gi.cprintf (ent, PRINT_HIGH, "Remote hashlist loaded.\n");
+    gi.cprintf(ent, PRINT_HIGH, "Remote hashlist loaded.\n");
 }
