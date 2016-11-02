2712aea1bcc32ed3331df28ef762bb53b4e70211
Joe Reid <joe@joereid.com>
2016-11-02 06:14:33 -0400

    formatting...

--- zb_acexcp.c
+++ zb_acexcp.c
@@ -16,7 +16,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-*/
+ */
 
 //
 // q2admin
@@ -29,39 +29,35 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "g_local.h"
 #include "fopen.h"
 
-qboolean ReadRemoteAnticheatExceptionFile(char *bfname)
-{
+qboolean ReadRemoteAnticheatExceptionFile(char *bfname) {
     URL_FILE *handle;
     FILE *outf;
 
     sprintf(buffer, "%s/%s", moddir, ANTICHEATEXCEPTIONLOCALFILE);
 
     // copy from url line by line with fgets //
-    outf=fopen(buffer,"w");
-    if(!outf)
-    {
-	gi.dprintf ("Error opening local anticheat exception file.\n");
-	return FALSE;
+    outf = fopen(buffer, "w");
+    if (!outf) {
+        gi.dprintf("Error opening local anticheat exception file.\n");
+        return FALSE;
     }
 
     handle = url_fopen(bfname, "r");
-    if(!handle)
-    {
-	gi.dprintf ("Error opening remote anticheat exception file.\n");
-	fclose(outf);
-	return FALSE;
+    if (!handle) {
+        gi.dprintf("Error opening remote anticheat exception file.\n");
+        fclose(outf);
+        return FALSE;
     }
 
-    while(!url_feof(handle))
-    {
-        if(!url_fgets(buffer,sizeof(buffer),handle)) {
-		// if it did timeout we are not trying again forever... - hifi
-		gi.dprintf("Timeout while waiting for reply.\n");
-		url_fclose(handle);
-		fclose(outf);
-		return FALSE;
-	}
-        fwrite(buffer,1,strlen(buffer),outf);
+    while (!url_feof(handle)) {
+        if (!url_fgets(buffer, sizeof (buffer), handle)) {
+            // if it did timeout we are not trying again forever... - hifi
+            gi.dprintf("Timeout while waiting for reply.\n");
+            url_fclose(handle);
+            fclose(outf);
+            return FALSE;
+        }
+        fwrite(buffer, 1, strlen(buffer), outf);
     }
 
     url_fclose(handle);
@@ -71,53 +67,45 @@ qboolean ReadRemoteAnticheatExceptionFile(char *bfname)
 
 // download up to date anticheat config file including all execptions for r1ch
 // ugly code.. ;x
-void getR1chExceptionList(void)
-{
+
+void getR1chExceptionList(void) {
     char cfgAnticheatList_enabled[100];
     q2a_strcpy(cfgAnticheatList_enabled, q2adminanticheat_enable->string);
 
-        if ( cfgAnticheatList_enabled[0] == '1')
-        {
-	    qboolean ret;
-            char cfgAnticheatRemoteList[100];
+    if (cfgAnticheatList_enabled[0] == '1') {
+        qboolean ret;
+        char cfgAnticheatRemoteList[100];
 
-            if(!q2adminanticheat_file || isBlank(q2adminanticheat_file->string))
-                    {
-                        q2a_strcpy(cfgAnticheatRemoteList, ANTICHEATEXCEPTIONREMOTEFILE);
-                    }
-            else
-                    {
-                        q2a_strcpy(cfgAnticheatRemoteList, q2adminanticheat_file->string);
-                    }
-
-            ret = ReadRemoteAnticheatExceptionFile(cfgAnticheatRemoteList);
-
-            if(!ret)
-                {
-                        gi.dprintf ("WARNING: " ANTICHEATEXCEPTIONREMOTEFILE " could not be found\n");
-                        logEvent(LT_INTERNALWARN, 0, NULL, ANTICHEATEXCEPTIONREMOTEFILE " could not be found", IW_BANSETUPLOAD, 0.0);
-                }
+        if (!q2adminanticheat_file || isBlank(q2adminanticheat_file->string)) {
+            q2a_strcpy(cfgAnticheatRemoteList, ANTICHEATEXCEPTIONREMOTEFILE);
+        } else {
+            q2a_strcpy(cfgAnticheatRemoteList, q2adminanticheat_file->string);
         }
+
+        ret = ReadRemoteAnticheatExceptionFile(cfgAnticheatRemoteList);
+
+        if (!ret) {
+            gi.dprintf("WARNING: " ANTICHEATEXCEPTIONREMOTEFILE " could not be found\n");
+            logEvent(LT_INTERNALWARN, 0, NULL, ANTICHEATEXCEPTIONREMOTEFILE " could not be found", IW_BANSETUPLOAD, 0.0);
+        }
+    }
 }
 
-void loadexceptionlist(void)
-{
+void loadexceptionlist(void) {
     char cfgAnticheatList_enabled[100];
     q2a_strcpy(cfgAnticheatList_enabled, q2adminanticheat_enable->string);
-        if ( cfgAnticheatList_enabled[0] == '1')
-	{
-	    // flush cache, which really sux
-            //q2a_strcpy(buffer, "fsflushcache\n");
-	    //gi.AddCommandString(buffer);
-	    getR1chExceptionList();
-    	    // execute exception list even if the download was not succeeded, since there is probably an old version available.
-            q2a_strcpy(buffer, "exec " ANTICHEATEXCEPTIONLOCALFILE "\n");
-	    gi.AddCommandString(buffer);
-	}
+    if (cfgAnticheatList_enabled[0] == '1') {
+        // flush cache, which really sux
+        //q2a_strcpy(buffer, "fsflushcache\n");
+        //gi.AddCommandString(buffer);
+        getR1chExceptionList();
+        // execute exception list even if the download was not succeeded, since there is probably an old version available.
+        q2a_strcpy(buffer, "exec " ANTICHEATEXCEPTIONLOCALFILE "\n");
+        gi.AddCommandString(buffer);
+    }
 }
 
-void reloadexceptionlistRun(int startarg, edict_t *ent, int client)
-{
+void reloadexceptionlistRun(int startarg, edict_t *ent, int client) {
     loadexceptionlist();
-    gi.cprintf (ent, PRINT_HIGH, "Exceptionlist loaded.\n");
+    gi.cprintf(ent, PRINT_HIGH, "Exceptionlist loaded.\n");
 }
