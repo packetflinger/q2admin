2440baf7cf33da21273ba81e458369f689bb8c3c
Joe Reid <joe@joereid.com>
2018-10-24 18:09:51 -0400

    Using Skullers platform specific stuff

--- g_main.c
+++ g_main.c
@@ -849,7 +849,7 @@ Returns a pointer to the structure with all entry points
 and global variables
 =================
  */
-game_export_t *GetGameAPI(game_import_t *import) {
+q_exported game_export_t *GetGameAPI(game_import_t *import) {
     GAMEAPI *getapi;
 #ifdef __GNUC__
     int loadtype;
