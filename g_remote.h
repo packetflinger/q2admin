ee1a3b5bc25b2c440fa10574987b1baac1fdaa25
Joe Reid <joe@joereid.com>
2020-12-30 19:32:36 -0500

    still cleaning up

--- g_remote.h
+++ g_remote.h
@@ -26,20 +26,20 @@
 #define CURFRAME        (remote.frame_number)
 
 // arg in seconds
-#define FUTURE_FRAME(t)    (CURFRAME + SECS_TO_FRAMES(t))
+#define FUTURE_FRAME(t) (CURFRAME + SECS_TO_FRAMES(t))
 
 
 // Remote Admin flags
-#define RFL_FRAGS      1 << 0 	// 1
-#define RFL_CHAT       1 << 1	// 2
-#define RFL_TELEPORT   1 << 2	// 4
-#define RFL_INVITE     1 << 3	// 8
-#define RFL_FIND       1 << 4	// 16
-#define RFL_WHOIS      1 << 5	// 32
-#define RFL_DEBUG      1 << 11	// 1024
-#define RFL(f)      ((remote.flags & RFL_##f) != 0)
+#define RFL_FRAGS       1 << 0    // 1
+#define RFL_CHAT        1 << 1    // 2
+#define RFL_TELEPORT    1 << 2    // 4
+#define RFL_INVITE      1 << 3    // 8
+#define RFL_FIND        1 << 4    // 16
+#define RFL_WHOIS       1 << 5    // 32
+#define RFL_DEBUG       1 << 11   // 1024
+#define RFL(f)          ((remote.flags & RFL_##f) != 0)
 
-#define MAX_MSG_LEN		1000
+#define MAX_MSG_LEN     1000
 
 #define PING_FREQ_SECS  10
 #define PING_MISS_MAX   3
@@ -49,142 +49,156 @@
 #define AESKEY_LEN      16  // bytes, 128 bits
 #define AESBLOCK_LEN    16  // bytes, 128 bits
 
+
 /**
  * The various states of the remote admin connection
  */
 typedef enum {
     RA_STATE_DISABLED,      // not using RA at all
-	RA_STATE_DISCONNECTED,  // will try to connect when possible
-	RA_STATE_CONNECTING,    // mid connection
-	RA_STATE_CONNECTED,     // connected
-	RA_STATE_TRUSTED        // authenticated and ready to go
+    RA_STATE_DISCONNECTED,  // will try to connect when possible
+    RA_STATE_CONNECTING,    // mid connection
+    RA_STATE_CONNECTED,     // connected
+    RA_STATE_TRUSTED        // authenticated and ready to go
 } ra_state_t;
 
 #define STATE(s)    (remote.state == RA_STATE_##s)
 
 typedef struct {
-	byte      data[QUEUE_SIZE];
-	size_t    length;
-	uint32_t  index;    // for reading
+    byte      data[QUEUE_SIZE];
+    size_t    length;
+    uint32_t  index;    // for reading
 } message_queue_t;
+
 
 /**
  * For pinging the server, if no reply after x frames, assuming
  * connection is broken and reconnect.
  */
 typedef struct {
-	qboolean  waiting;      // we sent a ping, waiting for a pong
-	uint32_t  frame_sent;   // when it was sent
-	uint32_t  frame_next;   // when to send the next one
-	uint8_t   miss_count;   // how many sent without a reply
+    qboolean  waiting;      // we sent a ping, waiting for a pong
+    uint32_t  frame_sent;   // when it was sent
+    uint32_t  frame_next;   // when to send the next one
+    uint8_t   miss_count;   // how many sent without a reply
 } ping_t;
+
 
 /**
  * Authenticating with a Remote Admin server is a 4-way handshake.
  * These describe those levels
  */
 typedef enum {
-	RA_AUTH_SENT_CL_NONCE,
-	RA_AUTH_REC_KEY,
-	RA_AUTH_SENT_SV_NONCE,
-	RA_AUTH_REC_ACK,
+    RA_AUTH_SENT_CL_NONCE,
+    RA_AUTH_REC_KEY,
+    RA_AUTH_SENT_SV_NONCE,
+    RA_AUTH_REC_ACK,
 } ra_auth_t;
 
+
+/**
+ * Connection specific stuff. Also holds the asymmetric and
+ * symmetric encryption keys and nonces.
+ */
 typedef struct {
     uint32_t    socket;
     qboolean    ipv6;
-	qboolean    trusted;      // is the server trusted?
-	qboolean  encrypted;    // should we encrypt?
-	ra_auth_t authstage;
-	byte      cl_nonce[CHALLENGE_LEN];  // random data
-	byte      sv_nonce[CHALLENGE_LEN];  // random data
-	byte      aeskey[AESKEY_LEN];       // shared encryption key (128bit)
-	byte      iv[AESBLOCK_LEN];         // aes block size
-	RSA       *rsa_pu;      // our public key
-	RSA       *rsa_pr;      // our private key
-	RSA       *rsa_sv_pu;   // RA server's public key
-	EVP_CIPHER_CTX  *ctx;   // sym encryption context
-	fd_set           set_r;    // read
-	fd_set           set_w;    // write
-	fd_set           set_e;    // error
+    qboolean    trusted;      // is the server trusted?
+    qboolean    encrypted;    // should we encrypt?
+    ra_auth_t   authstage;
+
+    // auth and encryption stuff
+    byte    cl_nonce[CHALLENGE_LEN];  // random data
+    byte    sv_nonce[CHALLENGE_LEN];  // random data
+    byte    aeskey[AESKEY_LEN];       // shared encryption key (128bit)
+    byte    iv[AESBLOCK_LEN];         // aes block size
+
+    RSA     *rsa_pu;      // our public key
+    RSA     *rsa_pr;      // our private key
+    RSA     *rsa_sv_pu;   // RA server's public key
+
+    EVP_CIPHER_CTX *ctx;  // sym encryption context
+
+    fd_set  set_r;    // read
+    fd_set  set_w;    // write
+    fd_set  set_e;    // error
 } ra_connection_t;
 
 /**
  * Holds all info and state about the remote admin connection
  */
 typedef struct {
-	ra_state_t       state;
-	ra_connection_t  connection;
-	uint32_t         connect_retry_frame;
-	uint32_t         connection_attempts;
-	uint32_t         connected_frame;  // the frame when we connected
-	uint32_t         socket;
-
-
-	fd_set           set_e;    // error
-	struct addrinfo  *addr;
-	uint32_t         flags;
-	uint32_t         frame_number;
-	char             mapname[32];
-	uint8_t          maxclients;
-	uint16_t         port;
-	message_queue_t  queue;
-	message_queue_t  queue_in;
-	ping_t           ping;
+    ra_state_t       state;
+    ra_connection_t  connection;
+    uint32_t         connect_retry_frame;
+    uint32_t         connection_attempts;
+    uint32_t         connected_frame;  // the frame when we connected
+    uint32_t         socket;
+    struct addrinfo  *addr;
+    uint32_t         flags;
+    uint32_t         frame_number;
+    char             mapname[32];
+    uint8_t          maxclients;
+    uint16_t         port;
+    message_queue_t  queue;     // messages outgoing to RA server
+    message_queue_t  queue_in;  // messages incoming from RA server
+    ping_t           ping;
 } remote_t;
+
 
 /**
  * Major client (q2server) to server (q2admin server)
  * commands.
  */
 typedef enum {
-	CMD_NULL,
-	CMD_HELLO,
-	CMD_QUIT,          // server quit
-	CMD_CONNECT,       // player
-	CMD_DISCONNECT,    // player
-	CMD_PLAYERLIST,
-	CMD_PLAYERUPDATE,
-	CMD_PRINT,
-	CMD_COMMAND,       // teleport, invite, etc
-	CMD_PLAYERS,
-	CMD_FRAG,          // someone fragged someone else
-	CMD_MAP,           // map changed
-	CMD_PING,          //
-	CMD_AUTH
+    CMD_NULL,
+    CMD_HELLO,
+    CMD_QUIT,          // server quit
+    CMD_CONNECT,       // player
+    CMD_DISCONNECT,    // player
+    CMD_PLAYERLIST,
+    CMD_PLAYERUPDATE,
+    CMD_PRINT,
+    CMD_COMMAND,       // teleport, invite, etc
+    CMD_PLAYERS,
+    CMD_FRAG,          // someone fragged someone else
+    CMD_MAP,           // map changed
+    CMD_PING,          //
+    CMD_AUTH
 } ra_client_cmd_t;
+
 
 /**
  * Server to client commands
  */
 typedef enum {
-	SCMD_NULL,
-	SCMD_HELLOACK,
-	SCMD_ERROR,
-	SCMD_PONG,
-	SCMD_COMMAND,
-	SCMD_SAYCLIENT,
-	SCMD_SAYALL,
+    SCMD_NULL,
+    SCMD_HELLOACK,
+    SCMD_ERROR,
+    SCMD_PONG,
+    SCMD_COMMAND,
+    SCMD_SAYCLIENT,
+    SCMD_SAYALL,
 } ra_server_cmd_t;
+
 
 /**
  * Sub commands. These are initiated by players
  */
 typedef enum {
-	CMD_COMMAND_TELEPORT,
-	CMD_COMMAND_INVITE,
-	CMD_COMMAND_WHOIS
+    CMD_COMMAND_TELEPORT,
+    CMD_COMMAND_INVITE,
+    CMD_COMMAND_WHOIS
 } remote_cmd_command_t;
 
-void 		RA_Send(void);
-void		RA_Init(void);
-void		RA_Shutdown(void);
-void		RA_RunFrame(void);
-void		RA_Register(void);
-void		RA_Unregister(void);
-void		RA_PlayerConnect(edict_t *ent);
-void		RA_PlayerDisconnect(edict_t *ent);
-void		RA_PlayerCommand(edict_t *ent);
+
+void        RA_Send(void);
+void        RA_Init(void);
+void        RA_Shutdown(void);
+void        RA_RunFrame(void);
+void        RA_Register(void);
+void        RA_Unregister(void);
+void        RA_PlayerConnect(edict_t *ent);
+void        RA_PlayerDisconnect(edict_t *ent);
+void        RA_PlayerCommand(edict_t *ent);
 
 uint8_t     RA_ReadByte(void);
 uint16_t    RA_ReadShort(void);
