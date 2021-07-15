73a1e55f719524857348655d0696ef7d3360d883
Joe Reid <joe@joereid.com>
2021-07-15 09:22:55 -0400

    osx already defined "true" and "false"

--- q_shared.h
+++ q_shared.h
@@ -62,8 +62,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 typedef unsigned char byte;
 
 typedef enum {
-    false,
-	true
+	qfalse,
+	qtrue
 } qboolean;
 
 #ifndef NULL
