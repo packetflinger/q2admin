cadcc3be078f53bf24d94e0a33a17ef4f2d3c39c
Joe Reid <joe@joereid.com>
2023-11-14 22:27:56 -0500

    fix up verify server auth and add debug print #15

--- g_cloud.h
+++ g_cloud.h
@@ -264,6 +264,7 @@ void        debug_print(char *str);
 void        G_SHA256Hash(byte *dest, byte *src, size_t src_len);
 void        cloudRun(int startarg, edict_t *ent, int client);
 void        CA_printf(char *fmt, ...);
+void        CA_dprintf(char *fmt, ...);
 void        ReadCloudConfigFile(char *filename);
 size_t      G_PublicEncrypt(EVP_PKEY *key, byte *out, byte *in, size_t inlen);
 
