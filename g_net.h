23809c06ccc7d22b83cb2ddc73d23305f1a13452
Joe Reid <joe@joereid.com>
2024-03-21 14:01:05 -0400

    Somehow missed a semicolon... #16

--- g_net.h
+++ g_net.h
@@ -27,7 +27,7 @@ typedef struct netadr_s {
 } netadr_t;
 
 qboolean AddressesMatch(netadr_t *a1, netadr_t *a2);
-void AddressToString(char *dest, netadr_t *address, qboolean wrapv6, qboolean incport)
+void AddressToString(char *dest, netadr_t *address, qboolean wrapv6, qboolean incport);
 void ParseIP(netadr_t *addr, const char *ip);
 void ParseIPAddressBase(netadr_t *address, const char *ip);
 qboolean ValidIPAddress(netadr_t *addr);
