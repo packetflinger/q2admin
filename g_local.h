570a28fec3c309771fa9aff64818622dd26fa9a9
Joe Reid <joe@joereid.com>
2024-04-17 09:55:42 -0400

    Remove unused g_offsets.h and include

--- g_local.h
+++ g_local.h
@@ -38,7 +38,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "g_cloud.h"
 #include <ctype.h>
 #include "regex.h"
-#include "g_offsets.h"
 #include "g_json.h"
 #include "g_net.h"
 #include "g_anticheat.h"
