c023e8cd81ebdf873395c14414c0f5ecd9cbf66f
Joe Reid <joe@joereid.com>
2024-04-18 12:04:29 -0400

    Remove moveinfo_t from g_local.h

--- g_local.h
+++ g_local.h
@@ -112,35 +112,6 @@ extern edict_t *g_edicts;
 #define random()                       ((rand () & 0x7fff) / ((float)0x7fff))
 
 typedef struct {
-    // fixed data
-    vec3_t      start_origin;
-    vec3_t      start_angles;
-    vec3_t      end_origin;
-    vec3_t      end_angles;
-
-    int         sound_start;
-    int         sound_middle;
-    int         sound_end;
-
-    float       accel;
-    float       speed;
-    float       decel;
-    float       distance;
-
-    float       wait;
-
-    // state data
-    int         state;
-    vec3_t      dir;
-    float       current_speed;
-    float       move_speed;
-    float       next_speed;
-    float       remaining_distance;
-    float       decel_distance;
-    void        (*endfunc)(edict_t *);
-} moveinfo_t;
-
-typedef struct {
     void    (*aifunc)(edict_t *self, float dist);
     float   dist;
     void    (*thinkfunc)(edict_t *self);
