c9ba09ceb760b01e3f20ad2a557f3c41aec4e26f
Joe Reid <joe@joereid.com>
2024-05-15 10:18:12 -0400

    Removed unsigned from byte (giving it a try)

--- shared.h
+++ shared.h
@@ -48,7 +48,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #endif
 
 #define BIT(n)  (1U << (n))
-typedef unsigned char byte;
+typedef char byte;
 typedef enum {
     qfalse,
     qtrue
