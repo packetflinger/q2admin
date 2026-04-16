6efd468dd9eb1f48bde467e281ab181a1d6721b5
Joe Reid <joe@joereid.com>
2026-04-16 15:48:01 -0400

    long decimal format in printf

--- g_spawn.c
+++ g_spawn.c
@@ -232,7 +232,7 @@ void displayNextSpawn(edict_t *ent, int client, long spawncmd) {
                 gi.cprintf(ent, PRINT_HIGH, "%4ld SW:\"%s\"\n", spawncmd + 1, spawncmds[spawncmd].spawncmd);
                 break;
             case SPAWN_EX:
-                gi.cprintf(ent, PRINT_HIGH, "%4d EX:\"%s\"\n", spawncmd + 1, spawncmds[spawncmd].spawncmd);
+                gi.cprintf(ent, PRINT_HIGH, "%4ld EX:\"%s\"\n", spawncmd + 1, spawncmds[spawncmd].spawncmd);
                 break;
             case SPAWN_RE:
                 gi.cprintf(ent, PRINT_HIGH, "%4d RE:\"%s\"\n", spawncmd + 1, spawncmds[spawncmd].spawncmd);
