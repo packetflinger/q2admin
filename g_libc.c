848aced10cf464238d44a4d8b08cbed80938ca6f
Joe Reid <joe@joereid.com>
2026-09-10 14:23:06 -0400

    Swap q2a_strcpy for Q_snprintf in libc stuff

--- g_libc.c
+++ g_libc.c
@@ -335,7 +335,7 @@ static const char *q2a_inet_ntop4 (const u_char *src, char *dst, socklen_t size)
         //__set_errno (ENOSPC);
         return (NULL);
     }
-    return q2a_strcpy(dst, tmp);
+    return Q_snprintf(dst, sizeof(dst), "%s", tmp);
 }
 
 /* const char *
@@ -430,5 +430,5 @@ static const char *q2a_inet_ntop6 (const u_char *src, char *dst, socklen_t size)
         //__set_errno (ENOSPC);
         return (NULL);
     }
-    return q2a_strcpy(dst, tmp);
+    return Q_snprintf(dst, sizeof(dst), "%s", tmp);
 }
